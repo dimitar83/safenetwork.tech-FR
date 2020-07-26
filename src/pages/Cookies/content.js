cde491dc5f1e1a71b4c4bcb48374ffe7081ede10
dimitar83 <38901442+dimitar83@users.noreply.github.com>
2020-07-26 06:15:31 +0300

    Update content.js

--- src/pages/Cookies/content.js
+++ src/pages/Cookies/content.js
@@ -1,8 +1,8 @@
 export default {
-  title: 'No Cookie Policy',
+  title: 'Aucune politique de cookies',
   desc: [
-    '[MaidSafe.net](https://maidsafe.net/) Limited understands that your privacy is important to you and that you care about how your personal data is used. We respect and value the privacy of everyone who visits this website so with that in mind, we don’t use cookies on this website.',
-    'No session cookie; no persistent cookie; no HttpOnly cookie; no secure cookie; no Third-party cookies. Seriously… NO COOKIES!'
+    '[MaidSafe.net](https://maidsafe.net/) Limited comprend que votre vie privée est importante pour vous et que vous vous souciez de la façon dont vos données personnelles sont utilisées. Nous respectons et apprécions la vie privée de tous ceux qui visitent ce site Web, c’est pourquoi nous n’utilisons pas de cookies sur ce site Web.',
+    'Aucun cookie de session; pas de cookie persistant; aucun cookie HttpOnly; pas de cookie sécurisé; pas de cookies tiers. Sérieusement… PAS DE COOKIES!'
   ],
   topics: []
 }
